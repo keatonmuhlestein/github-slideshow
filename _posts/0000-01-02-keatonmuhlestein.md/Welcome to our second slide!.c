93e1e619525f7edef6666358e81579f32b6185c0
Keaton Muhlestein <keatonmuhlestein@mail.weber.edu>
2019-01-22 20:04:57 -0700

    Use the left arrow to go back!

---
+++ _posts/0000-01-02-keatonmuhlestein.md/Welcome to our second slide!.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Welcome to our second slide!.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 07:45:43 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Keaton Muhlestein (), keatonmuhlestein@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    printf("Use the left arrow to go back!\n");
+
+    return 0;
+}
+// Function Definitions
+
+
